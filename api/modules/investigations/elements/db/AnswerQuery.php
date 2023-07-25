db7c0a03ed1289e4fec1da4d764eaf0bd90510c0
Kyle Pratuch <kyle@castironcoding.com>
2023-07-24 20:33:55 -0700

    [F] Add AnswerQuery element class

--- api/modules/investigations/elements/db/AnswerQuery.php
+++ api/modules/investigations/elements/db/AnswerQuery.php
@@ -10,13 +10,51 @@ use craft\elements\db\ElementQuery;
  */
 class AnswerQuery extends ElementQuery
 {
+    public $user;
+    public $question;
+    public $data;
+
+    public function user($value): self
+    {
+        $this->user = $value;
+        return $this;
+    }
+
+    public function question($value): self
+    {
+        $this->question = $value;
+        return $this;
+    }
+
+    public function data($value): self
+    {
+        $this->data = $value;
+        return $this;
+    }
+
     protected function beforePrepare(): bool
     {
-        // todo: join the `answers` table
-        // $this->joinElementTable('answers');
+        // JOIN our 'answers' table
+        $this->joinElementTable('answers');
 
-        // todo: apply any custom query params
-        // ...
+        // SELECT the `user` and `question` columns:
+        $this->query->select([
+            'answers.user',
+            'answers.question',
+            'answers.data'
+        ]);
+
+        if ($this->user) {
+            $this->subQuery->andWhere(Db::parseParam('answers.user', $this->user));
+        }
+
+        if ($this->question) {
+            $this->subQuery->andWhere(Db::parseParam('answers.question', $this->question));
+        }
+
+        if ($this->data) {
+            $this->subQuery->andWhere(Db::parseParam('answers.data', $this->data));
+        }
 
         return parent::beforePrepare();
     }
